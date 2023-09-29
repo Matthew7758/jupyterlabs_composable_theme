8c75cf1dd432cc3a6658495576abb3f579b14123
Matthew7758 <entei678@gmail.com>
2023-09-29 10:51:30 -0400

    First commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { IThemeManager } from '@jupyterlab/apputils';
+
+/**
+ * Initialization data for the jupyterlab-composable-theme extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlab-composable-theme:plugin',
+  description: 'A JupyterLab theme for Composable Analytics.',
+  autoStart: true,
+  requires: [IThemeManager],
+  activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
+    console.log('JupyterLab extension jupyterlab-composable-theme is activated!');
+    const style = 'jupyterlab-composable-theme/index.css';
+
+    manager.register({
+      name: 'jupyterlab-composable-theme',
+      isLight: true,
+      load: () => manager.loadCSS(style),
+      unload: () => Promise.resolve(undefined)
+    });
+  }
+};
+
+export default plugin;
